306c709d5ce4d8088dbaffd0e2463dd50000ed25
John Walker <johnwalker@Johns-MacBook-Air.local>
2020-06-24 15:12:59 +0100

    wtf

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,7 +6,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
-        
+        if (query.toLowerCase().contains("what is your name")) {
+            return "John Walker";
+        }
         return "";
     }
 }
